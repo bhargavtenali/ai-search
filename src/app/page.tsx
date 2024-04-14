d4c4c3cb7d60fb46861f4600e5fb29b5f780973c
Bhargav Tenali <bhargavtenali@gmail.com>
2024-04-14 22:29:55 +0530

    wip: openai api call integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,187 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState, Fragment } from "react";
 import { CiSearch } from "react-icons/ci";
-import fieldsArray from "../../Fields.json";
+import { IoMdClose } from "react-icons/io";
+import axios from "axios";
+import { apiStatusConstants } from "../../constants";
+import Chip from "@/components/Chip";
 
 const Home = () => {
-  const wordsToUnderline: any = [];
-  fieldsArray.forEach((field) => {
-    wordsToUnderline.push(field.uiFieldName);
-    wordsToUnderline.push(field.filterFieldName);
-  });
+  const inputRef = useRef<HTMLInputElement>(null);
   const divRef = useRef<HTMLDivElement>(null);
-  let matchedWords = [];
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/api/home")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setMessage(data.message);
-  //     });
-  // }, []);
+  const [jsxElements, setJsxElements] = useState<any[]>([]);
+  const [userInput, setUserInput] = useState("");
+  const [selectedFields, setSelectedFields] = useState<any[]>([]);
 
-  const handleInputKeyDown = (e: any) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      e.stopPropagation();
-      renderInputWithUnderline();
-    }
-  };
+  const cache: any = useMemo(() => {
+    return {};
+  }, []);
 
-  const renderInputWithUnderline = () => {
-    // Create a regular expression pattern to match all phrases
-    const input = divRef.current?.innerText;
-    const pattern = new RegExp(wordsToUnderline.join("|"), "gi");
-    matchedWords = [];
-    // Replace all occurrences of the matched phrases with themselves wrapped in <u> tags
-    if (input) {
-      const newTextContent = input.replace(pattern, (matched) => {
-        matchedWords.push(matched);
-        return `<u>${matched}</u>`;
-      });
-      if (divRef.current) {
-        divRef.current.innerHTML = newTextContent;
+  const [openaiApiState, setOpenaiApiState] = useState({
+    errorMsg: "",
+    apiStatus: apiStatusConstants.initial,
+    matchedFields: [],
+    mongoDBQuery: [],
+    autocomplete: [],
+  });
+
+  const fetchDebounce = (callback: any, delay: any) => {
+    let timer: any;
+    return function () {
+      if (timer) {
+        clearTimeout(timer);
       }
-    }
+      timer = setTimeout(() => {
+        callback();
+      }, delay);
+    };
   };
 
+  const removeSelectedField = useCallback(
+    (subString: string) => {
+      const newSelectedFields = selectedFields.filter(
+        (field) => field.matchedSubString !== subString
+      );
+      setSelectedFields(newSelectedFields);
+    },
+    [selectedFields]
+  );
+
+  const debounceApiCall = useMemo(
+    () =>
+      fetchDebounce(async () => {
+        const userInput = inputRef?.current?.value;
+        const payload = { userInput };
+        const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/openai/search`;
+        setOpenaiApiState({
+          errorMsg: "",
+          apiStatus: apiStatusConstants.inProgress,
+          matchedFields: [],
+          mongoDBQuery: [],
+          autocomplete: [],
+        });
+        try {
+          let data;
+          if (!cache[userInput]) {
+            const response = await axios.post(apiUrl, payload);
+            data = response.data;
+            cache[userInput] = data;
+          } else {
+            data = cache[userInput];
+          }
+          setOpenaiApiState({
+            errorMsg: "",
+            apiStatus: apiStatusConstants.success,
+            matchedFields: data.message.matchedFields,
+            mongoDBQuery: data.message.mongoDBQuery,
+            autocomplete: data.message.autocomplete,
+          });
+          if (data.message.matchedFields.length > 0) {
+            const matchedFieldsArray = data.message.matchedFields.map(
+              (field: any) => field.matchedSubString
+            );
+            const regexPattern = "(" + matchedFieldsArray.join("|") + ")";
+            const regex = new RegExp(regexPattern, "gi");
+            // wrap matched phrases in CHIP
+            if (userInput) {
+              const splittedArray = userInput.split(regex);
+              const elementsArray = splittedArray.map((substr, index) => {
+                if (regex.test(substr)) {
+                  return (
+                    <Chip
+                      key={index}
+                      subString={substr}
+                      removeSelectedField={removeSelectedField}
+                    />
+                  );
+                }
+                return <span key={index}>{substr}</span>;
+              });
+              if (divRef.current) {
+                divRef.current.innerHTML = "";
+                setJsxElements((prevState) => [...elementsArray]);
+                setSelectedFields(data.message.matchedFields);
+              }
+            }
+          }
+        } catch (error) {
+          console.error(error);
+          setOpenaiApiState({
+            errorMsg: `Error logging in due to ${error}`,
+            apiStatus: apiStatusConstants.failure,
+            matchedFields: [],
+            mongoDBQuery: [],
+            autocomplete: [],
+          });
+        }
+      }, 1000),
+    [removeSelectedField, cache]
+  );
+
+  const handleInputChange = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setUserInput(e.target.value);
+    //use debouncing and do openai call
+    debounceApiCall();
+  };
+  console.log(jsxElements, "jsxElements");
+  console.log(selectedFields, "selectedFields");
+
+  const renderInputField = () => {
+    if (
+      openaiApiState.apiStatus === apiStatusConstants.success &&
+      jsxElements.length > 0
+    ) {
+      return (
+        <div
+          ref={divRef}
+          className="flex justify-start items-center h-[50px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full ps-10 pe-10 p-2.5"
+        >
+          {jsxElements}
+        </div>
+      );
+    } else {
+      return (
+        <input
+          value={userInput}
+          ref={inputRef}
+          className="flex justify-start items-center h-[50px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full ps-10 pe-10 p-2.5"
+          onChange={handleInputChange}
+        />
+      );
+    }
+  };
   return (
     <div className="bg-container">
       <div className="w-full h-full flex justify-center items-center">
-        <div className="relative w-[60%]">
+        <div
+          className="relative w-[60%]"
+          onKeyDown={() =>
+            setOpenaiApiState({
+              errorMsg: "",
+              apiStatus: apiStatusConstants.initial,
+              matchedFields: [],
+              mongoDBQuery: [],
+              autocomplete: [],
+            })
+          }
+        >
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <CiSearch />
           </div>
+          {renderInputField()}
           <div
-            ref={divRef}
-            contentEditable="true"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-            onKeyDown={handleInputKeyDown}
-          />
+            className="absolute inset-y-0 end-0 flex items-center pe-3 cursor-pointer"
+            onClick={(e) => {
+              setUserInput("");
+              if (divRef.current) divRef.current.innerHTML = "";
+            }}
+          >
+            <IoMdClose />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Chip/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
+
+const Chip = ({ subString, removeSelectedField }: any) => {
+  const [renderPlainText, setRenderPlainText] = useState(false);
+  const clickHandler = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setRenderPlainText(true);
+    removeSelectedField(subString);
+  };
+  if (!renderPlainText) {
+    return (
+      <div className="inline-flex">
+        <span>&nbsp;</span>
+        <div className="rounded-lg flex justify-start align-center w-auto bg-slate-300 px-1 py-1 gap-1 cursor-pointer">
+          <span>{subString}</span>
+          <div
+            className="flex justify-start items-center text-slate-600"
+            onClick={clickHandler}
+          >
+            <IoMdCloseCircle />
+          </div>
+        </div>
+        <span>&nbsp;</span>
+      </div>
+    );
+  } else {
+    return <span>{` ${subString} `}</span>;
+  }
+};
+
+export default Chip;
